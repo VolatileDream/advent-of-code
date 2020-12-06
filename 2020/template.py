72526a14759a43d1866163d327555d7e5d0a7019
Gianni Gambetti <gianni@gambetti.ca>
2020-12-06 15:58:10 +0100

    add main template for 2020

---
+++ 2020/template.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import argparse
+import collections
+import re
+import sys
+import typing
+
+
+def load_file(filename):
+  contents = []
+  with open(filename, 'r') as f:
+    for line in f:
+      line = line.strip() # remove trailing newline
+      contents.append(line)
+  return contents
+
+
+def load_groups(filename):
+  # because some files follow this format instead.
+  contents = []
+  acc = [] 
+  for line in load_file(filename):
+    if line:
+      acc.append(line)
+    else:
+      contents.append(acc)
+      acc = []
+
+  if acc:
+    contents.append(line)
+
+  return contents
+
+
+def part1(things):
+  pass
+
+
+def part2(things):
+  pass
+
+
+def main(filename):
+  things = load_file(filename)
+
+  print("part 1:", part1(things))
+  print("part 2:", part2(things))
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('input', nargs='?', default='-')
+
+  args = parser.parse_args(sys.argv[1:])
+  main(args.input)

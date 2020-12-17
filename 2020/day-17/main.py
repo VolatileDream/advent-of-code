04c8c23d13b30d34f57b2121e1a2da4f75c8c0bf
Gianni Gambetti <gianni@gambetti.ca>
2020-12-17 18:27:31 +0100

    cleanup 2020 day 17

--- 2020/day-17/main.py
+++ 2020/day-17/main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import collections
+import functools
 import re
 import sys
 import typing
@@ -22,48 +23,103 @@ def any_item(iterable, default=None):
   return default
 
 
-class Position(typing.NamedTuple):
-  x: int
-  y: int
-  z: int
+class Coordinate:
+  def __init__(self, dimensions):
+    self.values = dimensions
+    self.__hash = None
+
+  def __repr__(self):
+    return "Coordinate({})".format(", ".join([str(i) for i in self.values]))
+
+  @staticmethod
+  def zero(length):
+    return Coordinate([0] * length)
+
+  def get_zero(self):
+    return Coordinate.zero(len(self))
+
+  @staticmethod
+  def unit(length):
+    return Coordinate([1] * length)
+
+  def get_unit(self):
+    return Coordinate.unit(len(self))
+
+  def __len__(self):
+    return len(self.values)
+
+  def __update__(self, func, other):
+    assert len(self) == len(other)
+    return Coordinate(list(map(func, zip(self.values, other.values))))
+
+  def __hash__(self):
+    if self.__hash is None:
+      self.__hash = self.__compute_hash()
+    return self.__hash
+
+  def __compute_hash(self):
+    # Much of this reflects python's tuple hashing:
+    # https://github.com/python/cpython/blob/eb48a451e3844185b9a8751c9badffbddc89689d/Objects/tupleobject.c#L407
+    h = 2870177450012600261
+    # the hash attempts to capture the position of the items
+    for i, v in enumerate(self.values):
+      # cyclic shift, using 17 & 47 to go through all positions (eventually)
+      h += hash(v) * 14029467366897019727
+      h = 0xffffffffffffffff & ((h << 31) | (h >> 33))
+      h *= 11400714785074694791
+
+    h += len(self) ^ 2870177450012600261;
+
+    return h
 
   def __neg__(self):
-    return Position(-self.x, -self.y, -self.z)
+    return Coordinate(list(map(lambda a: -a, self.values)))
 
   def __sub__(self, other):
-    return -other + self
+    return self.__update__(lambda a: a[0] - a[1], other)
 
   def __add__(self, other):
-    return Position(self.x + other.x, self.y + other.y, self.z + other.z)
+    return self.__update__(lambda a: a[0] + a[1], other)
 
-  @staticmethod
-  def unit():
-    return Position(1, 1, 1)
-
-  @staticmethod
-  def do_coords(p1, p2, func):
-    if p1 is None:
-      return p2
-    elif p2 is None:
-      return p1
-    x1, y1, z1 = p1
-    x2, y2, z2 = p2
-    return Position(func(x1, x2), func(y1, y2), func(z1, z2))
+  def __eq__(self, other):
+    if len(self) != len(other):
+      return False
+    for s, o in zip(self.values, other.values):
+      if s != o:
+        return False
+    return True
 
   def min(self, other):
-    return Position.do_coords(self, other, min)
+    if other is None:
+      return self
+    return self.__update__(min, other)
 
   def max(self, other):
-    return Position.do_coords(self, other, max)
+    if other is None:
+      return self
+    return self.__update__(max, other)
 
   @staticmethod
-  def iterate_range(p1, p2):
-    l_x, l_y, l_z = Position.min(p1, p2)
-    h_x, h_y, h_z = Position.max(p1, p2)
-    for z in range(l_z, h_z + 1):
-      for y in range(l_y, h_y + 1):
-        for x in range(l_x, h_x + 1):
-          yield Position(x, y, z)
+  def __r(lower, higher):
+    if len(lower) == 0:
+      yield []
+      return
+    l, *lrest = lower
+    h, *hrest = higher
+
+    for v in range(l, h+1):
+      for r in Coordinate.__r(lrest, hrest):
+        yield [v] + r
+
+  @staticmethod
+  def range(p1, p2):
+    lower = p1.min(p2)
+    higher = p1.max(p2)
+    assert lower == p1
+    assert higher == p2
+
+    for c in Coordinate.__r(lower.values, higher.values):
+      yield Coordinate(c)
 
 
 # dict with keys limited to True & False
@@ -75,6 +131,15 @@ class ConwayND:
     self.min = None
 
   @staticmethod
+  def from_lines2(lines, ext):
+    state = ConwayND(Coordinate)
+    for y, l in enumerate(lines):
+      for x, c in enumerate(l):
+        if c == '#':
+          state[Coordinate([x, y] + ext)] = True
+    return state
+
+  @staticmethod
   def from_lines(lines, CoordClass, **defaults):
     state = ConwayND(CoordClass)
     for y, l in enumerate(lines):
@@ -83,16 +148,10 @@ class ConwayND:
           state[CoordClass(x=x, y=y, **defaults)] = True
     return state
 
-  def __update_minmax__(self, key=None):
-    if key:
-      # incremental
-      self.min = key.min(self.min)
-      self.max = key.max(self.max)
-    else:
-      # full update
-      self.min = self.max = any_item(self.grid)
-      for p in grid:
-        self.__update_minmax__(p)
+  def __update_minmax__(self, key):
+    # incremental
+    self.min = key.min(self.min)
+    self.max = key.max(self.max)
 
   def __getitem__(self, key):
     return key in self.grid
@@ -106,7 +165,8 @@ class ConwayND:
 
   def step(self, adj_func, active_func):
     updated = ConwayND(self.clz)
-    for p in self.clz.iterate_range(self.min - self.clz.unit(), self.max + self.clz.unit()):
+    u = self.min.get_unit()
+    for p in self.clz.range(self.min - u, self.max + u):
       adj = set(adj_func(p))
       count = len(adj.intersection(self.grid))
       updated[p] = active_func(self[p], count)
@@ -116,29 +176,10 @@ class ConwayND:
   def bounds(self):
     return (self.min, self.max)
 
-  def __repr__(self):
-    lower, higher = self.bounds()
-    if lower is None or higher is None:
-      return ""
-    l_x, l_y, l_z = lower
-    h_x, h_y, h_z = higher
-    layers = ""
-    for z in range(l_z, h_z + 1):
-      layers += "z={}\n".format(str(z))
-      for y in range(l_y, h_y + 1):
-        for x in range(l_x, h_x + 1):
-          c = '.'
-          if Position(x, y, z) in self.grid:
-            c = '#'
-          layers += c
-        layers += '\n'
-      layers += '\n'
-    return layers
-
 
 def adjacent(p):
-  u = p.unit()
-  for adj in p.iterate_range(p - u, p + u):
+  u = p.get_unit()
+  for adj in p.range(p - u, p + u):
     if adj == p:
       continue
     yield adj
@@ -153,7 +194,7 @@ def active(state, count):
 
 
 def part1(things):
-  things = ConwayND.from_lines(things, CoordClass=Position, z=0)
+  things = ConwayND.from_lines2(things, [0])
   for i in range(6):
     print("iteration", i)
     #print(repr(things))
@@ -162,54 +203,8 @@ def part1(things):
   return len(things.grid) 
 
 
-class P4(typing.NamedTuple):
-  x: int
-  y: int
-  z: int
-  w: int
-
-  def __neg__(self):
-    return P4(-self.x, -self.y, -self.z, -self.w)
-
-  def __sub__(self, other):
-    return -other + self
-
-  def __add__(self, other):
-    return P4(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w)
-
-  @staticmethod
-  def unit():
-    return P4(1, 1, 1, 1)
-
-  @staticmethod
-  def do_coords(p1, p2, func):
-    if p1 is None:
-      return p2
-    elif p2 is None:
-      return p1
-    x1, y1, z1, w1 = p1
-    x2, y2, z2, w2 = p2
-    return P4(func(x1, x2), func(y1, y2), func(z1, z2), func(w1, w2)) 
-
-  def min(self, other):
-    return P4.do_coords(self, other, min)
-
-  def max(self, other):
-    return P4.do_coords(self, other, max)
-
-  @staticmethod
-  def iterate_range(p1, p2):
-    l_x, l_y, l_z, l_w = P4.min(p1, p2)
-    h_x, h_y, h_z, h_w = P4.max(p1, p2)
-    for z in range(l_z, h_z + 1):
-      for y in range(l_y, h_y + 1):
-        for x in range(l_x, h_x + 1):
-          for w in range(l_w, h_w + 1):
-            yield P4(x, y, z, w)
-
-
 def part2(things):
-  things = ConwayND.from_lines(things, CoordClass=P4, z=0, w=0)
+  things = ConwayND.from_lines2(things, [0, 0])
   for i in range(6):
     print("iteration", i)
     #print(repr(things))
@@ -226,6 +221,7 @@ def main(filename):
 
 
 if __name__ == "__main__":
+
   parser = argparse.ArgumentParser()
   parser.add_argument('input', nargs='?', default='/dev/stdin')
 

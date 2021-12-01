55733b55e4812bed4cf7fdec8695290acb1a1cca
Gianni Gambetti <gianni@gambetti.ca>
2021-12-01 12:37:11 -0500

    Uses flag file name
    Without providing a filename, use --input parameter

--- util.py
+++ util.py
@@ -11,14 +11,20 @@ from _.command_line.flags import Flag
 FLAG_input = Flag.str("input", short="i", default="/dev/stdin", description="Puzzle input file.")
 
 
-def load_input(filename):
+def load_input(filename=None):
+  if not filename:
+    filename = FLAG_input.value
+
   contents = []
   with open(filename, 'r') as f:
     for line in f:
-      yield line.strip() # remove trailing newline
+      line = line.strip() # remove trailing newline
+      contents.append(line)
+
+  return contents
 
 
-def load_groups(filename):
+def load_groups(filename=None):
   # because some files follow this format instead.
   contents = []
   acc = [] 
